ea2dcbe00872eb38074d7e38d64cd89fec5e5bab
ce-Peard <cepeard.cep@gmail.com>
2024-09-01 13:41:44 +0200

    photo-container style

--- wp-content/themes/nathalie-mota/single-photos.php
+++ wp-content/themes/nathalie-mota/single-photos.php
@@ -15,37 +15,39 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post(); ?>
-			<div class="photo-container"> 
-				<div class="photo-details-container"> 
-					<div class="photo-details"> 
-						<h2><?php the_title(); ?></h2>
-						<p>Référence : <?php echo get_field('reference'); ?></p>
-						<p>Catégorie : <?php echo strip_tags( get_the_term_list( get_the_ID(), 'categorie' ) );?></p>
-						<p>Format : <?php echo strip_tags( get_the_term_list( get_the_ID(), 'format' ) );?></p>
-						<p>Type : <?php echo get_field('type'); ?></p>
-						<p>Date : <?php echo the_time('Y'); ?></p>
-					</div> 
+			<div class="main-photo-container"> 
+				<div class="photo-container"> 
+					<div class="photo-details-container"> 
+						<div class="photo-details"> 
+							<h2><?php the_title(); ?></h2>
+							<p>Référence : <?php echo get_field('reference'); ?></p>
+							<p>Catégorie : <?php echo strip_tags( get_the_term_list( get_the_ID(), 'categorie' ) );?></p>
+							<p>Format : <?php echo strip_tags( get_the_term_list( get_the_ID(), 'format' ) );?></p>
+							<p>Type : <?php echo get_field('type'); ?></p>
+							<p>Date : <?php echo the_time('Y'); ?></p>
+						</div> 
+					</div>
+					<div class="photo-thumbnail">
+						<?php the_post_thumbnail(); ?>
+					</div> <
 				</div>
-				<div class="photo-thumbnail">
-					<?php the_post_thumbnail(); ?>
-				</div> <
-			</div>
-			<div class="photo-contact">
-				<div class="photo-contact-content">
-					<p>Cette photo vous intéresse ?</p>
-					<?php get_template_part('template-parts/contact', 'modale');?>
-				</div>
-				<div class="photo-navigation">
-					<div class="photo-navigation-content">
-						<p><</p>
-						<p>></p>
+				<div class="photo-contact">
+					<div class="photo-contact-content">
+						<p>Cette photo vous intéresse ?</p>
+						<?php get_template_part('template-parts/contact', 'modale');?>
+					</div>
+					<div class="photo-navigation">
+						<div class="photo-navigation-content">
+							<p><</p>
+							<p>></p>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="vous-aimerez-aussi">
-				<h3>Vous aimerez aussi</h3>
-			</div>
-			<div class="photo-related">
+				<div class="vous-aimerez-aussi">
+					<h3>Vous aimerez aussi</h3>
+				</div>
+				<div class="photo-related">
+				</div>
 			</div>
 		<?php endwhile;?>
 
