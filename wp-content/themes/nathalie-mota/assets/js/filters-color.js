b51f0047a38e68f6bc24f4f8642079343eaac933
ce-Peard <cepeard.cep@gmail.com>
2024-10-17 19:25:48 +0200

    filtres : hover & active different colors

--- wp-content/themes/nathalie-mota/assets/js/filters-color.js
+++ wp-content/themes/nathalie-mota/assets/js/filters-color.js
@@ -4,20 +4,35 @@ jQuery(function($) {
     // Initialiser Select2 sur les éléments <select>
     $('#menu1-categories, #menu2-formats, #menu3-tri').select2({
         minimumResultsForSearch: Infinity // Cache la barre de recherche
+    }).on('select2:select', function(e) {
+        var $select = $(this);
+        var selectedId = e.params.data.id;
+        
+        // Marquer toutes les options comme "previously-selected"
+        $select.find('option').addClass('previously-selected');
+        
+        // Retirer la classe de l'option actuellement sélectionnée
+        $select.find('option[value="' + selectedId + '"]').removeClass('previously-selected');
+        
+        // Appliquer les classes aux éléments générés par Select2
+        setTimeout(function() {
+            $('.select2-results__options li').each(function() {
+                var $option = $(this);
+                var optionId = $option.attr('id');
+                var originalOption = $('#' + optionId.replace('select2-', '').replace('-result-', '-'));
+                
+                if (originalOption.hasClass('previously-selected')) {
+                    $option.addClass('previously-selected');
+                } else {
+                    $option.removeClass('previously-selected');
+                }
+            });
+        }, 0);
     });
 
-    // Gérer les changements de filtre
-    $('#menu1-categories, #menu2-formats, #menu3-tri').on('change', function() {
-        console.log('Filtre changé:', $(this).attr('id'), $(this).val());
-    });
-
-    // Réinitialiser le sélecteur lorsque l'option vide est sélectionnée
-    $('#menu1-categories, #menu2-formats, #menu3-tri').on('select2:select', function(e) {
-        var data = e.params.data;
-        if (data.id === "reset") { // Vérifie si l'option vide est sélectionnée
-            $(this).val('').trigger('change'); // Réinitialise le sélecteur
-            clearFilters(); // Appel à une fonction pour effacer les filtres
-        }
+    // Ajouter un événement pour marquer les options comme visitées
+    $(document).on('click', '.select2-results__option', function() {
+        $(this).addClass('visited');
     });
 });
 
